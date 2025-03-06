42dad6e185e72d4697e112aac412f774dfc0b935
jovininasc <josevini03@gmail.com>
2025-03-06 14:06:29 -0300

    terminei o projeto da API

---
+++ src/controllers/tasksController.js
@@ -0,0 +1,36 @@
+const req = require('express/lib/request');
+const taskModel = require('../models/tasksModel');
+
+const getAll  = async (_request, response) =>{
+
+    const tasks = await taskModel.getAll();
+    return response.status(200).json(tasks);
+}
+
+const createTasks = async (request, response) => {
+    const createdTask = await taskModel.createTasks(request.body);
+
+   return response.status(201).json(createdTask);
+}
+
+const deleteTask = async(request, response) => {
+
+    const { id } = request.params;
+    await taskModel.deleteTask(id);
+    return response.status(204).json();
+}
+
+const updateTask = async(request, response) => {
+
+    const { id } = request.params;
+
+    await taskModel.updateTask(id, request.body);
+    return response.status(204).json()
+}
+
+module.exports = {
+    getAll,
+    createTasks,
+    deleteTask,
+    updateTask
+}
---
+++ src/router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const tasksController = require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddleware');
+
+const router = express.Router();
+
+router.get('/tasks', tasksController.getAll);
+router.post('/tasks',tasksMiddleware.validateTitle, tasksMiddleware.validateStatus, tasksController.createTasks);
+router.delete('/tasks/:id', tasksController.deleteTask);
+router.put('/tasks/:id', tasksMiddleware.validateTitle,tasksMiddleware.validateStatus, tasksController.updateTask);
+module.exports = router;
